68da0d24f50c601c454679b2bf929566bca2cd39
Matt Behrens <askedrelic@gmail.com>
2016-08-22 11:16:45 +0200

    make this more generci

--- bin/play.sh
+++ bin/play.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/opt/homebrew-cask/Caskroom/vlc/2.2.3/VLC.app/Contents/MacOS/VLC --intf=macosx "$@" > /dev/null 2>&1 &
+/Applications/VLC.app/Contents/MacOS/VLC --intf=macosx "$@" > /dev/null 2>&1 &
